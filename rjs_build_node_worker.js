fc92ed6f6974433b7d8cc40cd89296c5d4dacd2e
Patrik Meijer <patrik85@isis.vanderbilt.edu>
2016-12-16 17:53:15 -0600

    WIP Add worker source code.

---
+++ rjs_build_node_worker.js
@@ -0,0 +1,26 @@
+({
+    name: 'node_worker',
+    out: 'node_worker.classes.build.js',
+    baseUrl: './',
+    paths: {
+        blob: './node_modules/webgme/src/common/blob',
+        common: './node_modules/webgme/src/common',
+        executor: './node_modules/webgme/src/common/executor',
+        superagent: 'empty:',
+        fs: 'empty:',
+        util: 'empty:',
+        events: 'empty:',
+        path: 'empty:',
+        child_process: 'empty:', // jshint ignore:line
+        minimatch: 'empty:',
+        rimraf: 'empty:',
+        url: 'empty:',
+        q: 'empty:'
+    },
+    optimize: 'none',
+    include: ['./node_modules/requirejs/require'],
+    wrap: {
+        end: 'module.exports.require = require;\n' +
+             'module.exports.requirejs = requirejs;\nmodule.exports.define = define;\n'
+    }
+});
