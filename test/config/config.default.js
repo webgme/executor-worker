d744256f7da44d16add261e4bb090c2fa3a54259
Patrik Meijer <patrik85@isis.vanderbilt.edu>
2016-12-16 18:17:35 -0600

    WIP Add test context.

---
+++ test/config/config.default.js
@@ -0,0 +1,10 @@
+'use strict'
+var config = require('webgme/config/config.default'),
+    validateConfig = require('webgme/config/validator');
+
+config.mongo.uri = 'mongodb://127.0.0.1:27017/executor_worker';
+
+config.executor.enable = true;
+
+validateConfig(config);
+module.exports = config;
